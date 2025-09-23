925f30bbf7e69a79187caae8dfb88dbc3712da4b
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-09-23 18:27:25 +0200

    test classes

---
+++ src/test/java/com/mytests/spring/springprogrammaticbeansreposrouters/NoDbRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.mytests.spring.springprogrammaticbeansreposrouters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("no-db")
+public class NoDbRepositoryTest {
+
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void testGetAll() {
+        List<Person> allPersons = personRepository.findAllPersons();
+        assertNotNull(allPersons);
+        assertNotEquals(0, allPersons.size());
+    }
+
+    @Test
+    void testAddPerson() {
+        Person newPerson = new Person(20, "John", "Doe", 30);
+        personRepository.save(newPerson);
+        List<Person> allPersons = personRepository.findAllPersons();
+        assertEquals(4, allPersons.size());
+    }
+
+    @Test
+    void testFindById() {
+        Person person = personRepository.findById(1);
+        assertNotNull(person);
+        assertEquals("ivan", person.getFirstName());
+    }
+
+}
